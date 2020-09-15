31e0652ddb59811c9a1e3de34d76e6168058de35
anoopdeuskar <anoopdeuskar1@gmail.com>
2020-09-16 00:04:40 +0530

    chnaged code

--- src/main/java/com/coveros/demo/helloworld/HelloWorld.java
+++ src/main/java/com/coveros/demo/helloworld/HelloWorld.java
@@ -1,15 +1,13 @@
-package com.coveros.demo.helloworld;
+package com.test.HelloWorldMaven;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
-public class HelloWorld {
-
-  public static void main(final String[] args) {
-    final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
-    final LocalDateTime now = LocalDateTime.now();
-
-    System.out.println("Hello, World! The current time is " + dtf.format(now));
-  }
-
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
 }
