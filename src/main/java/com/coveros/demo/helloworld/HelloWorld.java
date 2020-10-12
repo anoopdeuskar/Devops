19dc7d2312e7f00456c4ab7b9adec4b9e2b04552
anoopdeuskar <anoopdeuskar1@gmail.com>
2020-10-12 13:00:44 +0530

    changed the code

--- src/main/java/com/coveros/demo/helloworld/HelloWorld.java
+++ src/main/java/com/coveros/demo/helloworld/HelloWorld.java
@@ -1,13 +1,5 @@
-package com.test.HelloWorldMaven;
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
     }
 }
